a6462f66d4f27f61a228f38cc4b217a441b4d6f0
MahesvaraCC <88294552+MahesvaraCC@users.noreply.github.com>
2021-08-22 23:38:38 -0300

    Entrega 1

--- js/login.js
+++ js/login.js
@@ -1,6 +1,31 @@
+/* Se utiliza un array como si fuera una base de datos */
+
+var Usuarios = [
+    {
+        user: ' '
+        password: ' '
+    }
+    {
+        user: ''
+        password: ''
+    }
+
+    {
+        user: 'x'
+        password: 'x'
+    }
+]
+
+function IngresoUsuarios(){
+    var user = document.getElementById('user').value
+    var password = document.getElementById('password').value
+    for (i=0; i< Usuarios.length; i++) {
+        if(user == Usuarios[i].user && password == Usuarios[i].password){
+            window.location.href = 'main.html';
+        }
+    }
+}
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
-
-});
--- js/products.js
+++ js/products.js
@@ -1,6 +1,45 @@
+var currentProductsArray = [];
+
+function showProductsList(ProductsList){
+    let htmlContentToAppend = "";
+    for(let i = 0; i < ProductsList.length; i++){
+        let product = ProductsList[i];
+
+            htmlContentToAppend += `
+            <a href="product-info.html" class="list-group-item list-group-item-action">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">`+ product.name +`</h4>
+                            <small class="text-muted">` + product.soldCount + ` vendidos</small>
+                        </div>
+                        <p class="mb-1">` + product.description + `</p>
+                        <div class="d-flex w-100 justify-content-between">
+                        <h5 class="mb-1"> Precio: `+ product.cost + ' ' + product.currency + `</h5>
+                    </div>
+                    </div>
+                </div>
+            </a>
+            `
+        }
+
+        document.getElementById("Products").innerHTML = htmlContentToAppend;
+    }
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
 
-});
+ document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+     if (resultObj.status ===     "ok")
+     {
+        currentProductsArray = resultObj.data;
+      //Muestro las categorías ordenadas
+      showProductsList(currentProductsArray);
+            }
+        });
+    });
