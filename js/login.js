a52e3232fd0a0ff60516c3d4cf4b8bf125c79e0c
mahesvaracc <88294552+mahesvaracc@users.noreply.github.com>
2021-09-05 21:42:33 -0300

    Entrega 2
    Entrega 2

--- js/login.js
+++ js/login.js
@@ -1,28 +1,17 @@
-/* Se utiliza un array como si fuera una base de datos */
+function GuardarDatos() {
+    localStorage.setItem('usuario', document.getElementById("user").value);
+    // guarda el nombre del usuario en local storage
+}
 
-var Usuarios = [
-    {
-        user: ' '
-        password: ' '
-    }
-    {
-        user: ''
-        password: ''
-    }
-
-    {
-        user: 'x'
-        password: 'x'
-    }
-]
-
-function IngresoUsuarios(){
-    var user = document.getElementById('user').value
-    var password = document.getElementById('password').value
-    for (i=0; i< Usuarios.length; i++) {
-        if(user == Usuarios[i].user && password == Usuarios[i].password){
-            window.location.href = 'main.html';
-        }
+function ValidarUsuario() {
+    let user = document.getElementById("user").value;
+    let password = document.getElementById("password").value;
+    if ((user !== "") && (password !== "")) {
+        GuardarDatos()
+        // Le pedimos a la función que tome los datos si los campos no están vacíos.
+        window.location.href = "main.html";
+    } else {
+        alert("Por favor complete los campos.");
     }
 }
 
--- js/products.js
+++ js/products.js
@@ -1,10 +1,65 @@
 var currentProductsArray = [];
+const ORDER_ASC_BY_PRICE = "LowHigh";
+const ORDER_DESC_BY_PRICE = "HighLow";
+const ORDER_BY_PROD_SOLD = "Relevance";
 
-function showProductsList(ProductsList){
+var currentSortCriteria = undefined;
+var minCost = undefined;
+var maxCost = undefined;
+
+function sortProducts(sortCriteria, array) {
+    let result = [];
+
+    if (sortCriteria === ORDER_BY_PROD_SOLD) {
+        /* Comparo los elementos del array de a dos y los reordena, sobreescribe el array original: */
+        result = array.sort(function (a, b) {
+            /* Parseo la cadena y obtengo un valor numérico*/
+            let aCount = parseInt(a.soldCount);
+            let bCount = parseInt(b.soldCount);
+            /* si el primer valor es superior al segundo, lo posiciona primero:  */
+            if (aCount > bCount) { return -1; }
+            /* si el primer valor es inferior al segundo, lo posiciona después:  */
+            if (aCount < bCount) { return 1; }
+        /* si el primer valor es igual al segundo no los mueve de lugar:  */
+            return 0;
+        });
+
+    } else if (sortCriteria === ORDER_ASC_BY_PRICE) {
+        result = array.sort(function (a, b) {
+            let aCost = parseInt(a.cost);
+            let bCost = parseInt(b.cost);
+
+            if (aCost < bCost) { return -1; }
+            if (aCost > bCost) { return 1; }
+            return 0;
+        });
+
+    } else if (sortCriteria === ORDER_DESC_BY_PRICE) {
+        result = array.sort(function (a, b) {
+            let aCost = parseInt(a.cost);
+            let bCost = parseInt(b.cost);
+
+            if (aCost > bCost) { return -1; }
+            if (aCost < bCost) { return 1; }
+            return 0;
+        
+        });
+    }
+
+    return result;
+}
+
+function showProductsList() {
     let htmlContentToAppend = "";
-    for(let i = 0; i < ProductsList.length; i++){
-        let product = ProductsList[i];
+    
+    for (let i = 0; i < currentProductsArray.length; i++) {
+        let product = currentProductsArray[i];
 
+/* Para que filtre uno a uno una vez se aplique la función anónima con los botones 
+pero igual me muestre los productos aunque no se haya filtrado:*/
+        if ( ((minCost == undefined) ||(minCost != undefined && parseInt(product.cost) >= minCost)) &&
+        ( (maxCost == undefined) || (maxCost != undefined && parseInt(product.cost) <= maxCost)))
+            { 
             htmlContentToAppend += `
             <a href="product-info.html" class="list-group-item list-group-item-action">
                 <div class="row">
@@ -13,7 +68,7 @@ function showProductsList(ProductsList){
                     </div>
                     <div class="col">
                         <div class="d-flex w-100 justify-content-between">
-                            <h4 class="mb-1">`+ product.name +`</h4>
+                            <h4 class="mb-1">`+ product.name + `</h4>
                             <small class="text-muted">` + product.soldCount + ` vendidos</small>
                         </div>
                         <p class="mb-1">` + product.description + `</p>
@@ -25,21 +80,87 @@ function showProductsList(ProductsList){
             </a>
             `
         }
-
-        document.getElementById("Products").innerHTML = htmlContentToAppend;
     }
 
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
+    document.getElementById("Products").innerHTML = htmlContentToAppend;
+}
 
- document.addEventListener("DOMContentLoaded", function(e){
-    getJSONData(PRODUCTS_URL).then(function(resultObj){
-     if (resultObj.status ===     "ok")
-     {
-        currentProductsArray = resultObj.data;
-      //Muestro las categorías ordenadas
-      showProductsList(currentProductsArray);
-            }
-        });
+function sortAndShowProducts(sortCriteria, productsArray) {
+    currentSortCriteria = sortCriteria;
+
+    if (productsArray != undefined) {
+        currentProductsArray = productsArray;
+    }
+
+    currentProductsArray = sortProducts(currentSortCriteria, currentProductsArray);
+
+    //Muestro los productos ordenados
+    showProductsList(currentProductsArray);
+}
+
+/* Funciones que se ejecutan una vez que se haya lanzado el evento de
+que el documento se encuentra cargado, es decir, se encuentran todos los
+elementos HTML presentes.
+Primero que nada, se muestran los productos ordenados por relevancia */
+
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCTS_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            sortAndShowProducts(ORDER_BY_PROD_SOLD, resultObj.data);
+        }
     });
+/* Si se da click en los botones ordena de a cuerdo a distintos criterios: */
+    document.getElementById("Relevancia").addEventListener("click", function () {
+        sortAndShowProducts(ORDER_BY_PROD_SOLD);
+    });
+
+    document.getElementById("sortAsc").addEventListener("click", function () {
+        sortAndShowProducts(ORDER_ASC_BY_PRICE);
+    });
+
+    document.getElementById("sortDesc").addEventListener("click", function () {
+        sortAndShowProducts(ORDER_DESC_BY_PRICE);
+    });
+/* Si se le asigna un rango de precio, filtra de acuerdo a eso, incluso cuando no se ingrese uno de los valores: */ 
+    document.getElementById("rangeFilterCost").addEventListener("click", function () {
+        //Obtengo los valores para filtrar por costo:
+        minCost = document.getElementById("rangeFilterCostMin").value;
+        maxCost = document.getElementById("rangeFilterCostMax").value;
+
+        if ((minCost != undefined) && (minCost != "") && (parseInt(minCost)) >= 0) {
+            minCost = parseInt(minCost);
+        }
+        else {
+            minCost = undefined;
+        }
+
+        if ((maxCost != undefined) && (maxCost != "") && (parseInt(maxCost)) >= 0) {
+            maxCost = parseInt(maxCost);
+        }
+        else {
+            maxCost = undefined;
+        }
+        showProductsList();
+    });
+
+ /* Para que me muestre la lista de productos sin filtrar una vez se limpie el filtro*/
+     document.getElementById("clearRangeFilter").addEventListener("click", function () {
+        document.getElementById("rangeFilterCostMin").value = "";
+        document.getElementById("rangeFilterCostMax").value = "";
+    
+        minCost = undefined;
+        maxCost = undefined;
+    
+        showProductsList();
+    });
+/* busqueda, pasa todo a minusculas para comparar */
+     document.getElementById('searchBar').addEventListener('keyup', function(e){
+        const search = e.target.value.toLowerCase();
+        const filteredProducts = currentProductsArray.filter(function(product){
+            return product.name.toLowerCase().includes(search)
+        })
+        console.log(filteredProducts)
+
+    sortAndShowProducts(currentSortCriteria, filteredProducts)
+    });
+}); 
